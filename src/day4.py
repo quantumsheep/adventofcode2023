a1690c5aaca6f49abceb503eebec76a21100631b
Nathanael DEMACON <ndemacon@scaleway.com>
2023-12-04 21:31:47 +0100

    feat(day4): simplify card parsing

--- src/day4.py
+++ src/day4.py
@@ -1,34 +1,16 @@
-from dataclasses import dataclass
-from typing import List
-
 from utils.input import read_day_input
 from utils.lo import intersect, split_strip
 
 
-@dataclass
 class Card:
-    id: int
-    winning_numbers: List[int]
-    my_numbers: List[int]
-    matching_numbers: int
-
-    count: int = 1
-
-    def parse(line: str) -> "Card":
-        card, numbers = line.split(": ")
-        card_id = card.split(" ")[-1]
+    def __init__(self, line: str):
+        _, numbers = line.split(": ")
         winning_numbers, my_numbers = numbers.split(" | ")
-        winning_numbers = [int(n) for n in split_strip(winning_numbers, " ")]
-        my_numbers = [int(n) for n in split_strip(my_numbers, " ")]
+        self.winning_numbers = [int(n) for n in split_strip(winning_numbers, " ")]
+        self.my_numbers = [int(n) for n in split_strip(my_numbers, " ")]
 
-        matching_numbers = intersect(winning_numbers, my_numbers)
-
-        return Card(
-            id=int(card_id),
-            winning_numbers=winning_numbers,
-            my_numbers=my_numbers,
-            matching_numbers=len(matching_numbers),
-        )
+        self.matching_numbers = len(intersect(self.winning_numbers, self.my_numbers))
+        self.count = 1
 
 
 def part1(data: str) -> int:
@@ -37,7 +19,7 @@ def part1(data: str) -> int:
     total = 0
 
     for line in lines:
-        card = Card.parse(line)
+        card = Card(line)
         if card.matching_numbers == 0:
             continue
 
@@ -48,7 +30,7 @@ def part1(data: str) -> int:
 
 def part2(data: str) -> int:
     lines = data.splitlines()
-    cards = [Card.parse(line) for line in lines]
+    cards = [Card(line) for line in lines]
 
     for i, card in enumerate(cards):
         if card.matching_numbers == 0:
